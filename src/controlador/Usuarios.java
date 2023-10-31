7903efb1c47aad54952e109f0664be01401820a5
Ryuzakiii-cl <ch.lhoest@duocuc.cl>
2023-10-31 14:33:07 -0300

    creacion de usuario

---
+++ src/controlador/Usuarios.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+/**
+ *
+ * @author Criiiis
+ */
+public class Usuarios {
+    private String nombre;
+    private String apellido;
+    private String rut;
+    private String direccion;
+    private String telefono;
+    private String correo;
+    private String rol;
+
+    public Usuarios() {
+    }
+
+    public Usuarios(String nombre, String apellido, String rut, String direccion, String telefono, String correo, String rol) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.rut = rut;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+    
+    
+    
+    
+    
+}//fin clase
