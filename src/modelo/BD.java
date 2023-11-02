55c14b4ea32b5e3e4bec0ca33119d40cba9f96f9
Ryuzakiii-cl <ch.lhoest@duocuc.cl>
2023-11-01 22:21:32 -0300

    Agregamos cositas

--- src/modelo/BD.java
+++ src/modelo/BD.java
@@ -3,7 +3,13 @@ package modelo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import vista.MedicoView;
+import vista.PacienteView;
+import vista.SecretariaView;
 
 public class BD {
     Connection conexion = null;
@@ -26,7 +32,62 @@ public class BD {
     
     
     
-    
+    // METODO PARA INICIAR SESION
+
+ public static void iniciarSesion(String correo, String contrasena) {
+        String rol = "";
+        String url = "jdbc:mysql://localhost:3306/usuarios_galeno";
+        String user = "root";
+        String password = "";
+        
+        try (Connection conn  = DriverManager.getConnection(url, user, password)) {
+            String query = "SELECT rol FROM usuarios WHERE correo = ? AND correo = ?";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, correo);
+            stmt.setString(2, correo);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                rol = rs.getString("rol");
+            } else {
+                JOptionPane.showMessageDialog(null, "Usuario Desconocido Cerrando Programa");
+                System.out.println("Credenciales inválidas. Inténtelo de nuevo.");
+                
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Redirigir según el rol
+        switch (rol) {
+            case "paciente":
+                // Abrir pantalla de vista para el Decano
+                PacienteView p = new PacienteView();
+                p.setVisible(true);
+                
+                break;
+            case "medico":
+                // Abrir pantalla de vista para el Profesor
+                MedicoView m = new MedicoView();
+                m.setVisible(true);
+                break;
+            case "secretaria":
+                // Abrir pantalla de vista para el Alumno
+                SecretariaView s = new SecretariaView();
+                s.setVisible(true);
+                break;
+            default:
+                System.out.println("Rol desconocido. No se puede abrir la pantalla de vista.");
+                break;
+            }
+        }// fin metodo iniciar sesion 
+ 
+ 
+ 
+ 
+ 
+ 
+
     
     
     
